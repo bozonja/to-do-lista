4cf71e00387374444c9e70a9aba46457caabe503
Josip Markovic <bozadesign@gmail.com>
2020-08-01 14:08:16 +0200

    Updates

---
+++ precache-manifest.688650636dce1e428928b29a0b585ab8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6584771a45ddb8989ed05600afeec4b5",
+    "url": "/to-do-lista/index.html"
+  },
+  {
+    "revision": "929496058280037e77da",
+    "url": "/to-do-lista/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c90057a037dd3f68edeb",
+    "url": "/to-do-lista/static/js/2.373a88d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-lista/static/js/2.373a88d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "929496058280037e77da",
+    "url": "/to-do-lista/static/js/main.c814e20f.chunk.js"
+  },
+  {
+    "revision": "4c8382505b2d8efff9fd",
+    "url": "/to-do-lista/static/js/runtime-main.d793e7f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-do-lista/static/media/logo.5d5d9eef.svg"
+  }
+]);
