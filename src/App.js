5884c00bc87a2d02bfb8d801140c61656af9f533
Josip Markovic <bozadesign@gmail.com>
2020-08-04 13:00:22 +0200

    add new component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import './komponente/Todolista'
+import Todolista from './komponente/Todolista';
 
 function App() {
   return (
     <div className="App">
       <h1>To Do Lista</h1>
+      <Todolista />
     </div>
   );
 }
---
+++ src/komponente/Todolista.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Todolista() {
+  return (
+    <div>
+      <p>TodoLista</p>
+    </div>
+  );
+}
+
+export default Todolista;
