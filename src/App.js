0f0bcfd0c3f2d0c4868f3f63992c44d9252f9dbb
Josip Markovic <bozadesign@gmail.com>
2020-08-04 13:29:50 +0200

    add state with hardcoded data, change app component to class component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import './komponente/Todolista'
 import Todolista from './komponente/Todolista';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>To Do Lista</h1>
-      <Todolista />
-    </div>
-  );
+class App extends Component {
+  state = {
+    todolista: [
+      {
+        id: 1,
+        naslov: "Baci smeće",
+        izvršeno: false
+      },
+
+      {
+        id: 2,
+        naslov: "Zovni starog",
+        izvršeno: false
+      },
+
+      {
+        id: 3,
+        naslov: "Otiđi po Alanu",
+        izvršeno: false
+      }
+    ]
+  }
+
+  render () {  
+    return (
+      <div className="App">
+        <h1>To Do Lista</h1>
+        <Todolista />
+      </div>
+    );
+  }
 }
 
 export default App;
