4f665880e586da7e8ac9a45c081e2386dd5959ab
Josip Markovic <bozadesign@gmail.com>
2020-08-06 17:25:30 +0200

    Add about component

--- src/komponente/AboutApp.js
+++ src/komponente/AboutApp.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function About() {
   return (
-   <div>
+   <div style={{backgroundColor: 'white', padding: '2rem'}}>
       <h1>About App</h1>
       <p>This app is created by Josip Markovic according tutorial "react crash 
         course" by Traversy Media. I did this so I can practice react</p>
--- src/komponente/Header.js
+++ src/komponente/Header.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <header className="header">
       <h1>ToDo List</h1>
+      <Link to="/" style={linkStyle}> Home </Link> | 
+      <Link to="/about" style={linkStyle}> About App </Link>
     </header>
   );
+}
+
+const linkStyle = {
+  color: 'white'
 }
 
 export default Header;
